e8408c50ebbd161ec661e78128eaee862d6e8c9d
asuka33 <suchaaaan33@gmail.com>
2025-05-30 10:39:22 +0900

    migrationとモデルの参照

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,8 @@
 
 ActiveRecord::Schema[7.2].define(version: 2025_05_29_042422) do
   create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
