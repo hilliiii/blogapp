151563df2da62f781a3fa1bcc05f97ee2bee0e97
asuka33 <suchaaaan33@gmail.com>
2025-06-07 09:24:23 +0900

    ユーザーと記事を紐付ける

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-    before_action :set_article, only: [:show, :edit, :update]
+    before_action :set_article, only: [:show]
     before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
     def index
@@ -9,14 +9,15 @@ class ArticlesController < ApplicationController
     end
 
     def show
+        @user_id = current_user.id
     end
 
     def new
-        @article = Article.new
+        @article = current_user.articles.build
     end
 
     def create
-        @article = Article.new(article_params)
+        @article = current_user.articles.build(article_params)
         if @article.save
             redirect_to article_path(@article), notice: '保存完了'
         else
@@ -26,9 +27,11 @@ class ArticlesController < ApplicationController
     end
 
     def edit
+        @article = current_user.articles.find(params[:id])
     end
 
     def update
+        @article = current_user.articles.find(params[:id])
         if @article.update(article_params)
             redirect_to article_path(@article), notice: '更新できました'
         else
@@ -38,7 +41,7 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        article = Article.find(params[:id])
+        article = current_user.articles.find(params[:id])
         article.destroy!
         redirect_to root_path, status: :see_other, notice: '削除に成功しました'
     end
